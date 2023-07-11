376836721a8c189396f1f9f7d0cfe22444b9c1d6
Seba9906 <troncoso.seb.99@gmail.com>
2023-07-11 10:14:38 -0300

    a

--- frontend/JS/productos.js
+++ frontend/JS/productos.js
@@ -61,7 +61,7 @@ createApp({
             fetch(this.url, options)
                 .then(function () {
                     alert('Registro grabado');
-                    window.location.href = '/index.html';
+                    window.location.href = 'index.html';
                 })
                 .catch(err => {
                     console.error(err);
